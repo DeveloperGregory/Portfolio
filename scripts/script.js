43594a72e779a2160fd680d1291b521cb96faedc
DeveloperGregory <69530744+DeveloperGregory@users.noreply.github.com>
2020-09-23 11:00:56 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,9 @@
-var headerIntro = document.getElementById('intro');
-var contentIntro = document.getElementById('intro-content');
 var headerProjects = document.getElementById('projects-div');
 var contentProjects = document.getElementById('projects-content');
 var headerSkills = document.getElementById('header-tech-skills');
 var contentTechSkills = document.getElementById('tech-skills')
 
 function collapse(){
-    contentIntro.className = 'heading-content';
     contentProjects.className = 'heading-content';
     contentTechSkills.className = 'heading-content';
     
@@ -17,6 +14,5 @@ function expand(selected){
     
 }
 
-headerIntro.addEventListener('click', function(){expand(contentIntro)});
 headerProjects.addEventListener('click', function(){expand(contentProjects)});
 headerSkills.addEventListener('click',function(){expand(contentTechSkills)});
